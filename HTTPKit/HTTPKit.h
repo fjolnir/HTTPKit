3e686855cdd2f1f848539f0f7fd5629e1b7bf90f
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-12-02 02:28:45 +0900

    Initial Commit

---
+++ HTTPKit/HTTPKit.h
@@ -0,0 +1,13 @@
+//
+//  HTTPKit.h
+//  HTTPKit
+//
+//  Created by Fjölnir Ásgeirsson on 12/2/12.
+//  Copyright (c) 2012 Fjölnir Ásgeirsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTTPKit : NSObject
+
+@end
---
+++ HTTPKit/HTTPKit.m
@@ -0,0 +1,13 @@
+//
+//  HTTPKit.m
+//  HTTPKit
+//
+//  Created by Fjölnir Ásgeirsson on 12/2/12.
+//  Copyright (c) 2012 Fjölnir Ásgeirsson. All rights reserved.
+//
+
+#import "HTTPKit.h"
+
+@implementation HTTPKit
+
+@end
