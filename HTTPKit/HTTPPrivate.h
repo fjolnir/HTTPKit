67bbff6f5b03ac7a857bc9e42e89a9296ec41c7f
Fjölnir Ásgeirsson <fjolnir@asgeirsson.is>
2012-12-04 00:48:04 +0900

    first commit

---
+++ HTTPKit/HTTPPrivate.h
@@ -0,0 +1,14 @@
+//
+//  HTTPPrivate.h
+//  HTTPKit
+//
+//  Created by Fjölnir Ásgeirsson on 12/2/12.
+//  Copyright (c) 2012 Fjölnir Ásgeirsson. All rights reserved.
+//
+
+#ifndef HTTPKit_HTTPPrivate_h
+#define HTTPKit_HTTPPrivate_h
+
+
+
+#endif
---
+++ test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by Fjölnir Ásgeirsson on 12/2/12.
+//  Copyright (c) 2012 Fjölnir Ásgeirsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
